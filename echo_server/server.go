bcff1488cc694f5a5ec082a65cd6023c2c6e20a5
aludwin1 <aludwin1@gmail.com>
2020-08-06 11:42:26 -0400

    Moved handler outside of main, added some validation logic for fun

--- echo_server/server.go
+++ echo_server/server.go
@@ -6,7 +6,23 @@ import (
 	"net/http"
 )
 
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/echo" { // Testing out path validation
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {  // Testing out r.Method
+		http.Error(w, "Method not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Request contents will be echoed here")
+}
+
 func main() {
+	http.HandleFunc("/echo", echoHandler)
+
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
