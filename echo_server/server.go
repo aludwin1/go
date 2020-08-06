4bce85f07884ac303d595eb79ef392c449b5df9d
aludwin1 <aludwin1@gmail.com>
2020-08-06 12:04:45 -0400

    Set up a web form and a route to handle form submission where the form's contents are echoed back to the user

--- echo_server/server.go
+++ echo_server/server.go
@@ -6,22 +6,28 @@ import (
 	"net/http"
 )
 
-func echoHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/echo" { // Testing out path validation
-		http.Error(w, "404 not found.", http.StatusNotFound)
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Error parsing the form you submitted. Err: %v", err)
 		return
 	}
 
-	if r.Method != "GET" {  // Testing out r.Method
+	if r.Method != "POST" {  // Testing out r.Method
 		http.Error(w, "Method not supported", http.StatusNotFound)
 		return
 	}
 
-	fmt.Fprintf(w, "Request contents will be echoed here")
+	fmt.Fprintf(w, "Post request successful\n")
+	message := r.FormValue("message")
+
+	fmt.Fprintf(w, "Your message said - %s\n", message)
 }
 
 func main() {
-	http.HandleFunc("/echo", echoHandler)
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
